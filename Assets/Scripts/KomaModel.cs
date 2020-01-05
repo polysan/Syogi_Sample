fa96c8815fa6537637abf93da653a4501c7bea06
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2020-01-05 10:59:43 +0900

    First Commit

---
+++ Assets/Scripts/KomaModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KomaModel : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public Sprite[] faces;
+    public Sprite komaBack;
+    public Sprite[] komaNaru;
+
+    public int cardIndex;
+    public int komaBackIndex;
+
+    public void ToggleFace(int showFace)　//外部アクセス可能なToggleFaceというメソッドの定義　引数に真偽値としてshowFaceを取る。
+    {
+        if (showFace == 0)
+        {
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else
+        {
+            if (showFace == 1)
+            {
+                spriteRenderer.sprite = komaNaru[komaBackIndex];
+            }
+            else
+            {
+                spriteRenderer.sprite = komaBack;
+            }
+        }
+    }
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
---
+++ Assets/Scripts/MouseOverAlways.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseOverAlways : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	CardFlipper flipper;
+	KomaModel cardModel;
+
+	private void Awake()
+	{
+		flipper = GetComponent<CardFlipper>();
+		cardModel = GetComponent<KomaModel>();
+	}
+	// オブジェクトの範囲内にマウスポインタが入った際に呼び出されます。
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		cardModel.ToggleFace(0);
+
+	}
+	// オブジェクトの範囲内からマウスポインタが出た際に呼び出されます。
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		flipper.FlipCard(cardModel.faces[cardModel.cardIndex], cardModel.komaBack, -1);
+	}
+}
